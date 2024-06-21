b86b30cca682f70452b9e065539cbcc876ba4d78
Zeynep Kıray <zeynepkiray@Zeyneps-MacBook-Pro.local>
2024-06-21 22:12:45 +0300

    updated fronted

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("index", views.index),
-    path("blogs", views.blogs, name ="blogs"),
-    path("blogs/<int:id>", views.blog_details, name="blog_details"), 
-    
+    path("index/", views.index, name ="index"),
+    path("blogs/", views.blogs, name="blogs"),
+    path("blogs/<int:id>/", views.blog_details, name="blog_details"),
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,5 +1,4 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -11,6 +10,7 @@ def index(req):
 def blogs(req):
     posts = Post.objects.all()
     return render(req, 'blog/blogs.html', {'posts': posts})
+
 def blog_details(req, id):
     post = get_object_or_404(Post, id=id)
-    return render(req, 'blog/blog_details.html', {'post': post})
+    return render(req, 'blog/details.html', {'post': post})
